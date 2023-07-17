03f813ee15a402fab55cf964d351c7875dcb9ea8
Araj Chalise <araj.chalise@gmail.com>
2023-07-17 12:47:21 +0545

    mymodule: changes for v0.1.0

---
+++ token.go
@@ -0,0 +1,77 @@
+package token
+
+import (
+	"fmt"
+	"strings"
+	"time"
+
+	jwt "github.com/dgrijalva/jwt-go"
+	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
+)
+
+const nullUUID = "00000000-0000-0000-0000-000000000000"
+
+func GenerateToken(user_id uuid.UUID, lifespan int, api_secret string) (string, error) {
+
+	claims := jwt.MapClaims{}
+	claims["authorized"] = true
+	claims["user_id"] = user_id
+	claims["exp"] = time.Now().Add(time.Hour * time.Duration(lifespan)).Unix()
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+
+	return token.SignedString(api_secret)
+
+}
+
+func TokenValid(c *gin.Context, api_secret []byte) error {
+	tokenString := ExtractToken(c)
+	_, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
+		}
+		return api_secret, nil
+	})
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func ExtractToken(c *gin.Context) string {
+	token := c.Query("token")
+	if token != "" {
+		return token
+	}
+	bearerToken := c.Request.Header.Get("Authorization")
+	if len(strings.Split(bearerToken, " ")) == 2 {
+		return strings.Split(bearerToken, " ")[1]
+	}
+	return ""
+}
+
+func ExtractTokenID(c *gin.Context, api_secret []byte) (uuid.UUID, error) {
+	nUUID, err := uuid.Parse(nullUUID)
+	if err != nil {
+		return uuid.UUID{}, err
+	}
+	tokenString := ExtractToken(c)
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
+		}
+		return api_secret, nil
+	})
+	if err != nil {
+		return nUUID, err
+	}
+	claims, ok := token.Claims.(jwt.MapClaims)
+	if ok && token.Valid {
+		userUUID, ok := claims["user_id"].(uuid.UUID)
+		if !ok {
+			return uuid.UUID{}, fmt.Errorf("Invalid user UUID ")
+		}
+		return userUUID, nil
+	}
+	return nUUID, fmt.Errorf("Invalid user token")
+}
